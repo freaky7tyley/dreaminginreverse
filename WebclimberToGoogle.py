29ff80e9bbc8f7389536bb00e1c81c4182930dd1
freaky7tyley <florian-huber@posteo.de>
2022-09-27 23:03:44 +0200

    feat(cocoburg)

--- WebclimberToGoogle.py
+++ WebclimberToGoogle.py
@@ -22,7 +22,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+SCOPES = ['https://www.googleapis.com/auth/calendar.events','https://www.googleapis.com/auth/calendar']
 
 iAm = 'Flo H.'
 myMail='florian.huber@kwpsoftware.de'
@@ -31,14 +31,38 @@ myMail='florian.huber@kwpsoftware.de'
 myOAuthPort=8105
 myOauthClientSecretFile="client_secret.json"
 
+myGreiKalender="cocoburgh"
+
 url ='https://157.webclimber.de/de/courseBooking'
 teachers = []
+googleService=None
+calID=None
 
-def EventExists(service,summary,id):
+def addNewCalendarIfNotExists(service,calendarTitle):
+    page_token = None
+
+    while True:
+        calendar_list = service.calendarList().list(pageToken=page_token).execute()
+        for calendar_list_entry in calendar_list['items']:
+            if calendar_list_entry['summary'] == calendarTitle:
+                return calendar_list_entry['id']
+        page_token = calendar_list.get('nextPageToken')
+        if not page_token:
+            break
+
+    new_calendar = {
+    'summary': calendarTitle,
+    'timeZone': 'Europe/Berlin'
+    }
+    created_calendar = service.calendars().insert(body=new_calendar).execute()
+
+    return created_calendar['id']
+
+def EventExists(service,calId,summary,id):
     now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
     tMax=datetime.utcnow().replace(month=datetime.now().month+3).isoformat()+ 'Z' 
     
-    events_result = service.events().list(calendarId='primary', timeMin=now, timeMax=tMax, singleEvents=True, orderBy='startTime').execute()
+    events_result = service.events().list(calendarId=calId, timeMin=now, timeMax=tMax, singleEvents=True, orderBy='startTime').execute()
     events = events_result.get('items', [])
     
     found = False
@@ -54,7 +78,39 @@ def EventExists(service,summary,id):
             print('Event does not exist yet.')
     return found
 
-def insertEventToGoogleCalendar(calID,starttime,endtime,summary,location,description,id):
+def insertEventToGoogleCalendar(service,calID,starttime,endtime,summary,location,description,id):
+    try:
+        if EventExists(service,calID,summary,id):
+            return
+    except:
+        pass
+    
+    event = CreateCalEvent(starttime, endtime, summary, location, description, id)
+    
+    try:
+        print('Inserting event')
+        event = service.events().insert(calendarId=calID, body=event).execute()
+        print ('Event created: %s' % (event.get('htmlLink')))
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+def CreateCalEvent(starttime, endtime, summary, location, description, id):
+    return {
+        'summary': summary,
+        'location': location,
+        'description': description+'\nKursnummer: '+id,
+        'start': {
+            'dateTime': starttime.isoformat(),
+            'timeZone': 'Europe/Berlin',
+        },
+        'end': {
+            'dateTime': endtime.isoformat(),
+            'timeZone': 'Europe/Berlin',
+        },
+        }
+
+def ConnectToGoogleAndCreateService():
     creds = None
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
@@ -69,55 +125,21 @@ def insertEventToGoogleCalendar(calID,starttime,endtime,summary,location,descrip
 
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
-    
-    try:
-        service = build('calendar', 'v3', credentials=creds)
-        try:
-            if EventExists(service,summary,id):
-                return
-        except:
-            pass
- 
-        print('Inserting event')
 
-        event = {
-        'summary': summary,
-        'location': location,
-        'description': description+'\nKursnummer: '+id,
-        'start': {
-            'dateTime': starttime.isoformat(),
-            'timeZone': 'Europe/Berlin',
-        },
-        'end': {
-            'dateTime': endtime.isoformat(),
-            'timeZone': 'Europe/Berlin',
-        },
-        }
+    service = build('calendar', 'v3', credentials=creds)
+    return service
 
-        event = service.events().insert(calendarId=calID, body=event).execute()
-        print ('Event created: %s' % (event.get('htmlLink')))
-
-    except HttpError as error:
-        print('An error occurred: %s' % error)
-
-
-def build_event_duration(summary, description, start, duration, location, url):
-    event = Event()
-    event.add('summary', summary)
-    event.add('description', description)
-    event.add('dtstart', start)
-    event.add('duration', timedelta(hours=duration))
-    event.add('dtstamp', datetime.now())
-    event.add('location', location)
-    event.add('url', url)
-
-    return event
-
-    
-def AddToGoogleCalendar(calID,start,duration_hours,title,url,id):
+def AddToGoogleCalendar(start,duration_hours,title,url,id):
     location="DAV Kletterzentrum Landshut, Ritter-von-Schoch Str.6, 84036 Landshut",
     end = start + timedelta(hours = duration_hours)
-    insertEventToGoogleCalendar(calID,start,end,title,location,url,id)
+    
+    global googleService
+    global calID
+    if googleService == None:
+        googleService = ConnectToGoogleAndCreateService()
+        calID = addNewCalendarIfNotExists(googleService,myGreiKalender)
+
+    insertEventToGoogleCalendar(googleService,calID,start,end,title,location,url,id)
 
 def getSoup(url):
     browser.go(url)
@@ -125,9 +147,6 @@ def getSoup(url):
     soup = BeautifulSoup(html,features="lxml")
     
     return soup
-
-def getOddEven(soup):
-    return soup.find_all(class_="odd") + soup.find_all(class_="even")
 
 def getTable(soup,tableattribute):
     data = []
@@ -170,6 +189,17 @@ def getDates(course):
     dates = course[1].split('\n')
     return dates
 
+def SetDateAndDurationHours(parsed_date, times):
+    hour_start=int(times[0].split(':')[0])
+    minute_start=int(times[0].split(':')[1])
+    hour_stop=int(times[1].split(':')[0])
+    minute_stop=int(times[1].split(':')[1])
+                        
+    parsed_date = parsed_date.replace(hour=hour_start, minute=minute_start)
+    DurationHours = (int(hour_stop)-int(hour_start)) + (int(minute_stop)-int(minute_start))/60
+
+    return parsed_date,DurationHours
+
 soup = getSoup(url)
 courseTypes = getCourseTypes(soup)
 
@@ -209,20 +239,13 @@ for courseType in courseTypes:
                     if '-' in myDate:
                         times = myDate.split('-')
                         
-                        hour_start=int(times[0].split(':')[0])
-                        minute_start=int(times[0].split(':')[1])
-                        hour_stop=int(times[1].split(':')[0])
-                        minute_stop=int(times[1].split(':')[1])
-                        
-                        parsed_date = parsed_date.replace(hour=hour_start, minute=minute_start)
-                        
-                        DurationHours = (int(hour_stop)-int(hour_start)) + (int(minute_stop)-int(minute_start))/60
+                        parsed_date, DurationHours = SetDateAndDurationHours(parsed_date, times)
                         
                         cId=id+'_'+str(id_suffix)
                         
                         print(cId,text,parsed_date,DurationHours,courseURL)
                     
-                        AddToGoogleCalendar(myMail,parsed_date,DurationHours,text,courseURL,cId)
+                        AddToGoogleCalendar(parsed_date,DurationHours,text,courseURL,cId)
                         
                         id_suffix=id_suffix+1
 
